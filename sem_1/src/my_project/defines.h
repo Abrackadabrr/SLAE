047fb2eb1b6c0dc37529ba0595d2c8c995fa69de
Abrackadabrr <iliynea2002@ya.ru>
2022-02-06 22:31:19 +0300

    addition defines

---
+++ sem_1/src/my_project/defines.h
@@ -0,0 +1,10 @@
+//
+// Created by evgen on 06.02.2022.
+//
+
+#ifndef SLAE_DEFINES_H
+#define SLAE_DEFINES_H
+
+#define ERR_INFO "\nFile: " << __FILE__ << "\nLine: " << __LINE__
+
+#endif //SLAE_DEFINES_H
