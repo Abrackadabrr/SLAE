6c91668db353737f612cfb0cdfbd3f9f10aa2556
Abrackadabrr <iliynea2002@ya.ru>
2022-02-12 00:46:33 +0300

    Add docs

--- src/my_project/solvers/ThreeDiagonalSolver.hpp
+++ src/my_project/solvers/ThreeDiagonalSolver.hpp
@@ -9,14 +9,19 @@
 #include "my_project/Defines.h"
 
 namespace Slae::Solvers {
-    /* @brief The method solves the system of equations using the sweep method
+    /** @brief The method solves the system of equations using the sweep method
     * Solves a system of linear algebraic equations using the sweep method. You can learn about the sweep method
     * from ... (source link)
     *
     * @param matrix threediagonal matrix
+     *
     * @param right_side column
     *
+    * @warning Matrix instance must have the property of diagonal dominance.
+    * If it have not, instead of real result of SLAE, you wil be given by nan or smth like that
+    *
     * @throw SlaeBaseExceptionCpp is thrown if the number of matrix rows and column height do not match
+    * @return The result of SLAE
     */
     std::vector<double>
     solveThreeDiagonal(const Slae::Matrix::ThreeDiagonalMatrix &matrix, const std::vector<double> &right_side);
--- tests/test_matrix.cpp
+++ tests/test_matrix.cpp
@@ -17,40 +17,15 @@ using std::endl;
  */
 TEST(threediagonalmatrix, matrix_passed)
 {
-    try {
-        int n = 4;
-        Slae::Matrix::ThreeDiagonalMatrix a = Slae::Matrix::ThreeDiagonalMatrix::ThreeDiagonal(n, 1, 3, 1);
-        std::vector<double> b{1, 1, 1, 2};
+    int n = 4;
+    Slae::Matrix::ThreeDiagonalMatrix a = Slae::Matrix::ThreeDiagonalMatrix::ThreeDiagonal(n, 1, 3, 1);
+    std::vector<double> b{1, 1, 1, 2};
 
-        std::vector<double> sol{14, 13, 2, 36};
-        sol = sol*(1./55);
-        std::vector<double> res = Slae::Solvers::solveThreeDiagonal(a,b);
-        for (int i = 0; i < n-1; ++i) {
-            ASSERT_NEAR(res[i], sol[i], 10e-10);
-        }
-        cout << res << endl;
+    std::vector<double> sol{14, 13, 2, 36};
+    sol = sol*(1./55);
+    std::vector<double> res = Slae::Solvers::solveThreeDiagonal(a,b);
+    for (int i = 0; i < n-1; ++i) {
+        ASSERT_NEAR(res[i], sol[i], 10e-10);
     }
-    catch (const Slae::SlaeBaseExceptionCpp &err) {
-        cout << err.what() << endl;
-    }
-}
-
-TEST(threediagonalmatrix, matrix_failure)
-{
-    try {
-        int n = 4;
-        Slae::Matrix::ThreeDiagonalMatrix a = Slae::Matrix::ThreeDiagonalMatrix::ThreeDiagonal(n, 1, 3, 1);
-        std::vector<double> b{1, 1, 1, 2};
-
-        std::vector<double> sol{12, 13, 2, 36};  // solution from lin above
-        sol = sol*(1./55);
-        std::vector<double> res = Slae::Solvers::solveThreeDiagonal(a,b);
-        for (int i = 0; i < n-1; ++i) {
-            ASSERT_NEAR(res[i], sol[i], 10e-10);
-        }
-        cout << res << endl;
-    }
-    catch (const Slae::SlaeBaseExceptionCpp &err) {
-        cout << err.what() << endl;
-    }
+    cout << res << endl;
 }
